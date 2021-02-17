21ddf59c92cc39f3f7b81dd05f735bd77de55ba0
Sannan <somitiger5@gmail.com>
2021-02-17 19:01:04 +0500

    Updates

---
+++ precache-manifest.47466afb0c221bc0a895dc1b56eebbc5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "81c9fada886b6ee46446",
+    "url": "/ath/static/css/main.827ad64b.chunk.css"
+  },
+  {
+    "revision": "81c9fada886b6ee46446",
+    "url": "/ath/static/js/main.81c9fada.chunk.js"
+  },
+  {
+    "revision": "ff7dcb57154887dd768a",
+    "url": "/ath/static/css/1.f0680be2.chunk.css"
+  },
+  {
+    "revision": "ff7dcb57154887dd768a",
+    "url": "/ath/static/js/1.ff7dcb57.chunk.js"
+  },
+  {
+    "revision": "a5495563de43a050a3f2",
+    "url": "/ath/static/js/runtime~main.a5495563.js"
+  },
+  {
+    "revision": "74e1c3eea6f6ddda6f0b19fa7d830b7d",
+    "url": "/ath/static/media/spin.74e1c3ee.svg"
+  },
+  {
+    "revision": "f4e64106c2249e89fe92aa75bec86afc",
+    "url": "/ath/static/media/bars.f4e64106.svg"
+  },
+  {
+    "revision": "4143001088bc0165d264f03b156398ae",
+    "url": "/ath/static/media/puff.41430010.svg"
+  },
+  {
+    "revision": "f8ea5dda2d2a3ad1844087d924d352ef",
+    "url": "/ath/static/media/dots.f8ea5dda.svg"
+  },
+  {
+    "revision": "d65d6b8d9ce266736b48e4d25cff9fd7",
+    "url": "/ath/static/media/vibe-logo.d65d6b8d.svg"
+  },
+  {
+    "revision": "05f9bc6d0460b0f780979f006a76d136",
+    "url": "/ath/static/media/laptop.05f9bc6d.jpeg"
+  },
+  {
+    "revision": "a56d13399ca5fce64c22ea391e352cc9",
+    "url": "/ath/static/media/apple.a56d1339.png"
+  },
+  {
+    "revision": "64cc8c4fae9886667bc59b39493c4b2d",
+    "url": "/ath/static/media/microsoft.64cc8c4f.png"
+  },
+  {
+    "revision": "8db0524040f5f8b3834c0eefe65aaca9",
+    "url": "/ath/static/media/avatar2.8db05240.jpeg"
+  },
+  {
+    "revision": "d39764fe69d38399c3f910794a58227f",
+    "url": "/ath/static/media/react-feature.d39764fe.svg"
+  },
+  {
+    "revision": "ef06be682e46b84ab0f5c70dd37c0c34",
+    "url": "/ath/static/media/sass-feature.ef06be68.svg"
+  },
+  {
+    "revision": "2fca69b6bb96516c28ab47656561a6cb",
+    "url": "/ath/static/media/bootstrap-feature.2fca69b6.svg"
+  },
+  {
+    "revision": "3a6289fc6dd93ee6b850c21e604ead26",
+    "url": "/ath/static/media/responsive-feature.3a6289fc.svg"
+  },
+  {
+    "revision": "d95f9fc1fd47929411b657a47bdfeffa",
+    "url": "/ath/index.html"
+  }
+];
